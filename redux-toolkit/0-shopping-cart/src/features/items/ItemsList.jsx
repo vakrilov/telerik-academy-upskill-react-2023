96674eb009dacc07718a6be5758d9dd1d92105a9
vakrilov <alexander.vakrilov@gmail.com>
2024-01-04 19:10:49 +0200

    item component

--- redux-toolkit/0-shopping-cart/src/features/items/ItemsList.jsx
+++ redux-toolkit/0-shopping-cart/src/features/items/ItemsList.jsx
@@ -5,30 +5,38 @@ import {
 } from "../shopping-cart/shoppingCartSlice";
 
 export const ItemsList = () => {
-  const dispatch = useDispatch();
   const items = useSelector((s) => s.items);
-  const cart = useSelector((s) => s.cart);
 
   return (
     <main className="panel">
       <h3>Items List</h3>
       <div className="panel items-list">
-        {items.map((item) => {
-          const disableRemove = !cart[item.id];
-          return (
-            <div className="item" key={item.id}>
-              <button
-                disabled={disableRemove}
-                onClick={() => dispatch(removeItemFromCart(item))}
-              >
-                -
-              </button>
-              {item.name}
-              <button onClick={() => dispatch(addItemToCart(item))}>+</button>
-            </div>
-          );
-        })}
+        {items.map(({ id }) => (
+          <Item key={id} id={id} />
+        ))}
       </div>
     </main>
   );
 };
+
+const Item = ({ id }) => {
+  const dispatch = useDispatch();
+
+  const item = useSelector((s) => s.items.find((item) => item.id === id));
+  const isItemInCart = useSelector((s) => !!s.cart[id]);
+
+  return (
+    <div className="item">
+      <button
+        disabled={!isItemInCart}
+        onClick={() => dispatch(removeItemFromCart(item))}
+      >
+        -
+      </button>
+      {item.name}
+      <button onClick={() => dispatch(addItemToCart(item))}>+</button>
+    </div>
+  );
+};
+
+// const Button = ({ children, onClick, isLoading, disabled }) => ()
